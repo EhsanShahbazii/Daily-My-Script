3609cdba6d60a2cdd9beef3098b92e917aba8926
Ehsan <ehsan.shahbazicode@gmail.com>
2023-09-12 18:07:13 +0330

    Update get_normal_string.js

--- get_normal_string.js
+++ get_normal_string.js
@@ -1,10 +1,13 @@
 /* https://castbox.fm/creator/channels
 my input name of songs are not correct (maybe!)
 so i write very simple script and do it for me.
+and generate hashtag for bio.
 */
 
+// name of the song
 const str = "Night_Drive_Wave_Mix_Atmospheric_Bass_Music_602EOIsSaJI_140";
 
+// generate hashtag with the name
 const generateHashTags = (str) => {
   let hashTags = [];
   str.split("_").forEach((item) => {
@@ -13,6 +16,7 @@ const generateHashTags = (str) => {
   return hashTags;
 };
 
+// remove _ and replace it with space
 const normalizing = (str) => {
   return str.replaceAll("_", " ");
 };
